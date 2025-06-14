b9405d6a0131e2d7141e5a043689a55e8a7787dd
Alkis <alkis.k1972@gmail.com>
2025-06-14 15:11:34 +0300

    end of day 1 final

--- week-1/counter-app/src/components/Counter.jsx
+++ week-1/counter-app/src/components/Counter.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'; // ✅ Βήμα 1: κάνε import το useState από τη React
+import { useState } from 'react'; // Βήμα 1: κάνε import το useState από τη React
 
 function App() {
-    // ✅ Βήμα 2: Δημιουργείς ένα κομμάτι state
+    // Βήμα 2: Δημιουργείς ένα κομμάτι state
     // 'count' είναι η τρέχουσα τιμή
     // 'setCount' είναι η συνάρτηση που τη μεταβάλλει
     const [count, setCount] = useState(0); // Αρχική τιμή: 0
 
-    // ✅ Βήμα 3: Η συνάρτηση που αυξάνει το count
+    // Βήμα 3: Η συνάρτηση που αυξάνει το count
     function handlePlusClick() {
         setCount(count + 1);
     }
     function handleMinusClick() {
         setCount(count - 1);
     }
+    const resetCount = () => {
+        setCount(0);
+    };
 
-    // ✅ Βήμα 4: Επιστρέφεις το UI
+    // Βήμα 4: Επιστρέφεις το UI
     return (
         <div>
             <h1>My Counter</h1>
             <p>Count = {count}.</p>
-            <button onClick={handlePlusClick}>Click for +</button>
+            <button onClick={handlePlusClick}>
+                Click for {'('}+{')'}
+            </button>
             <br></br>
             <button onClick={handleMinusClick}>
-                Click for {'('}-{')'}"
+                Click for {'('}-{')'}
             </button>
+            <br></br>
+            <button onClick={resetCount}>Reset counter</button>
         </div>
     );
 }
